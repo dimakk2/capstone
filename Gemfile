a7337a186e0114abfa4e56f41496e11917c30a7f
Dmitry Sablin <91dims@gmail.com>
2019-01-20 21:07:34 +0300

    Init

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.6'
+gem 'rails-api', '~>0.4', '>=0.4.0'
+gem 'rack-cors', '~>0.4', '>=0.4.0', :require => 'rack/cors'
+
+gem 'jbuilder', '~>2.0', '>=2.6.0'
+
+group :development do
+  gem 'spring', '~>2.0', '>=2.0.0'
+end
+
+group :development, :test do
+  gem 'tzinfo-data', :platforms=>[:mingw, :mswin, :x64_mingw, :jruby]
+  gem 'byebug'
+  gem 'httparty', '~>0.14', '>=0.14.0'
+
+  gem 'rspec-rails', '~>3.5', '>=3.5.2'
+end
+
+group :production do
+  gem 'rails_12factor', '~>0.0', '>= 0.0.3'
+end
+
+gem 'puma', '~>3.6', '>=3.6.0', :platforms => :ruby
+gem 'pg', '~>0.19', '>=0.19.0'
+gem 'mongoid', '~>5.1', '>=5.1.5'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Myapp::Application.config.secret_token = 'ba2376f7d007f55cc3d5d7c45264887c25b0b5fffec6c5aac08731a662225eca418799c9c12497c5942cd72c2e394734e1e6006e2498dc4f966da43290b6a181'
